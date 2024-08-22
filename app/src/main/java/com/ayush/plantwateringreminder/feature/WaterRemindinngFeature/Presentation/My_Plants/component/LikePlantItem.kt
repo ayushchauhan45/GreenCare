16762f2ff60f73e67befe28f7b932ed3ff61a82e
ayushchauhan45 <chauhanasc2004@gmail.com>
2024-08-22 19:09:45 +0530

    Add Like Plant Screen

---
+++ app/src/main/java/com/ayush/plantwateringreminder/feature/WaterRemindinngFeature/Presentation/My_Plants/component/LikePlantItem.kt
@@ -0,0 +1,94 @@
+package com.ayush.plantwateringreminder.feature.WaterRemindinngFeature.Presentation.My_Plants.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.ayush.plantwateringreminder.R
+import com.ayush.plantwateringreminder.feature.WaterRemindinngFeature.Domain.Model.PlantEntity
+
+@Composable
+fun LikePlantItem(
+    plant:PlantEntity,
+    modifier: Modifier = Modifier,
+){
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Row {
+            Box(
+                modifier = Modifier
+                    .weight(1.3f)
+                    .background(Color(0xFFC9D8CC))
+                    .fillMaxHeight()
+            ) {
+                AsyncImage(model = plant.defaultImageUrl, contentDescription = null,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(10.dp))
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .weight(2f)
+                    .background(Color(0xFFDAF3BF)) // White background for the text section
+                    .padding(10.dp)
+                    .fillMaxHeight()
+            ){
+                Column {
+                    Text(text = plant.commonName,
+                        color = Color(0xFF024405),
+                        fontSize = 16.sp,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        modifier = Modifier.align(Alignment.Start)
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Row {
+                        Image(painter = painterResource(id = R.drawable.watering_can),
+                            contentDescription = "watering Can")
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(text = plant.watering,
+                            color = Color(0xFF024405),
+                            fontSize = 14.sp
+                        )
+
+
+                    }
+
+                }
+
+
+            }
+        }
+
+
+    }
+
+}
